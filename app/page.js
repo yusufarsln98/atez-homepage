8b5efbe4e4c38bfade07c5391be01953c6e1528a
Yusuf Arslan <yusufarsln98@outlook.com>
2023-09-23 01:53:02 +0300

    home page design done

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import styles from "@/styles/Page.module.scss";
 import { useTheme } from "@/context/Theme.context";
-import Header from "@/components/Header";
+import Header from "@/components/header/Header";
 
 export default function Home() {
   const { theme } = useTheme();
@@ -15,7 +15,55 @@ export default function Home() {
       }}
     >
       <Header />
-      <div>home</div>
+      <div className={styles.home}>
+        <div className={styles.homeContainer}>
+          <div className={styles.homeText}>
+            <h1 className={styles.title}>
+              Yeni Nesil Uluslararası <br /> Ticaret ve Tedarik Zinciri
+              Teknolojileri
+            </h1>
+            <p className={styles.description}>
+              <span
+                style={{
+                  fontWeight: "bold",
+                }}
+              >
+                ATEZ
+              </span>{" "}
+              global ölçekli çözümler üretme hedefi ile çalışmalarını
+              sürdürmektedir.
+            </p>
+            <button>KEŞFET</button>
+          </div>
+          <div className={styles.jobCard}>
+            <h1>2023 Yaz Stajı</h1>
+            <p>
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt
+            </p>
+            <div className={styles.jobCardButtonContainer}>
+              <>BAŞVURMAK İÇİN TIKLA</>
+              <button>
+                <Image
+                  src="/icons/RightWhite.svg"
+                  alt="arrow-right"
+                  width={20}
+                  height={20}
+                />
+              </button>
+            </div>
+          </div>
+          <div className={styles.homeImage}>
+            <Image
+              src="/dummies/Home.png"
+              alt="hero"
+              width={1107 * 0.9}
+              height={960 * 0.9}
+              style={{ objectFit: "contain", zIndex: -1, marginTop: 100 }}
+            />
+          </div>
+        </div>
+      </div>
       <div>home</div>
       <div>home</div>
       <div></div>
--- context/Theme.config.js
+++ context/Theme.config.js
@@ -7,11 +7,9 @@ export const THEME = {
     "--blue": "#2699FB",
     "--blue-light": "#BCE0FD",
     "--home-bg": "rgb(82,59,93);",
-    "--home-gradient":
-      "linear-gradient(113deg, rgba(82,59,93,1) 0%, rgba(244,0,142,1) 100%);",
+    "--home-gradient": "linear-gradient(113deg, #523b5d 0%, #f4008e 100%);",
     "--card-bg": "rgb(0,147,255);",
-    "--card-gradient":
-      "linear-gradient(90deg, rgba(0,147,255,1) 0%, rgba(150,61,193,1) 100%);",
+    "--card-gradient": "linear-gradient(90deg, #0093FF 0%, #963DC1 100%);",
     "--footer-bg": "rgb(169,24,116);",
     "footer-gradient":
       "linear-gradient(0deg, rgba(169,24,116,1) 0%, rgba(85,12,58,1) 100%);",
