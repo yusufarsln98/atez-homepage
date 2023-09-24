6d3e54b4401f7025ea615481310eea732bd0428d
Yusuf Arslan <yusufarsln98@outlook.com>
2023-09-24 22:40:54 +0300

    biz kimiz modulu eklendi

--- app/page.js
+++ app/page.js
@@ -4,9 +4,31 @@ import Image from "next/image";
 import styles from "@/styles/Page.module.scss";
 import { useTheme } from "@/context/Theme.context";
 import Header from "@/components/header/Header";
+import { Home } from "@/components/home/Home";
 
-export default function Home() {
+// images
+const images = [
+  "/biz-kimiz/Hyper@2x.png",
+  "/biz-kimiz/Indy@2x.png",
+  "/biz-kimiz/Html@2x.png",
+  "/biz-kimiz/NodeJS@2x.png",
+  "/biz-kimiz/React@2x.png",
+  "/biz-kimiz/SQL@2x.png",
+  "/biz-kimiz/Ansible@2x.png",
+  "/biz-kimiz/GIT@2x.png",
+  "/biz-kimiz/Amazon@2x.png",
+  "/biz-kimiz/Docker@2x.png",
+  "/biz-kimiz/Kubernetes@2x.png",
+  "/biz-kimiz/Jenkins@2x.png",
+  "/biz-kimiz/Kafka@2x.png",
+  "/biz-kimiz/Redis@2x.png",
+  "/biz-kimiz/Postman@2x.png",
+  "/biz-kimiz/CentOS@2x.png",
+];
+
+export default function Page() {
   const { theme } = useTheme();
+
   return (
     <main
       className={styles.main}
@@ -15,56 +37,64 @@ export default function Home() {
       }}
     >
       <Header />
-      <div className={styles.home}>
-        <div className={styles.homeContainer}>
-          <div className={styles.homeText}>
-            <h1 className={styles.title}>
-              Yeni Nesil Uluslararası <br /> Ticaret ve Tedarik Zinciri
-              Teknolojileri
+      <Home />
+
+      <div className={styles.whoWeAre}>
+        <div className={styles.whoWeAreTextContainer}>
+          <div className={styles.title}>
+            <h1>
+              <span>BİZ KİMİ</span>
+              Z?
             </h1>
-            <p className={styles.description}>
-              <span
-                style={{
-                  fontWeight: "bold",
-                }}
-              >
-                ATEZ
-              </span>{" "}
+          </div>
+          <div className={styles.description}>
+            <p>
+              ATEZ 2017 Yılında Türkiye&apos;deki en büyük gümrük müşavirliği
+              firmalanndan biri olan Güler & Dinamik Gümrük Müşavirliği
+              firmasndan, uluslararası ticaret ile ilgili alanlarda faaliyet
+              gösteren tüm Özel sektör ile kamu kurum ve kuruluşlarının
+              çalışmalarının dijitalleştirilebilmesi için gerekli yazılımların
+              geliştirilmesi, mevcut yazılımların bakım ve destek
+              faaliyetlerinin sürdürülmesi amacı ile kurulmuştur. <br></br>
+              <br></br>
+              2019 Yılı Mayıs ayında Sanayi ve Teknoloji Bakanlığı nezdinde Blok
+              zincir projeleri (Alanında dünyada tek ve Türkiye&apos;nin de ilk
+              projesi olan Blok zincir Ticaret Platformu gibi) ile onaylı AR-GE
+              merkezi olma hakkını elde eden ATEZ Yazılım Teknolojileri A.Ş.,
               global ölçekli çözümler üretme hedefi ile çalışmalarını
               sürdürmektedir.
             </p>
-            <button>KEŞFET</button>
           </div>
-          <div className={styles.jobCard}>
-            <h1>2023 Yaz Stajı</h1>
-            <p>
-              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-              officia deserunt
-            </p>
-            <div className={styles.jobCardButtonContainer}>
-              <>BAŞVURMAK İÇİN TIKLA</>
-              <button>
-                <Image
-                  src="/icons/RightWhite.svg"
-                  alt="arrow-right"
-                  width={20}
-                  height={20}
-                />
-              </button>
+        </div>
+        <div className={styles.whoWeAreImageContainer}>
+          {images.map((image, index) => (
+            <div key={index} className={styles.imageCard}>
+              <Image
+                src={image}
+                alt="biz-kimiz"
+                width={100}
+                height={100}
+                layout="responsive"
+              />
             </div>
-          </div>
-          <div className={styles.homeImage}>
-            <Image
-              src="/dummies/Home.png"
-              alt="hero"
-              width={1107 * 0.9}
-              height={960 * 0.9}
-              style={{ objectFit: "contain", zIndex: -1, marginTop: 100 }}
-            />
+          ))}
+        </div>
+        <div className={styles.whoWeAreApplicationsContainer}>
+          <div className={styles.whoWeAreApplications}>
+            <p>BAŞVURULAR</p>
+            {/* button */}
+            <button>
+              {/* image */}
+              <Image
+                src={"/icons/Right.svg"}
+                alt="right"
+                width={20}
+                height={20}
+              />
+            </button>
           </div>
         </div>
       </div>
-      <div>home</div>
       <div>home</div>
       <div></div>
     </main>
